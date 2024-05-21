70a019794114db541c5bd3eae4caa6ad93007a73
mane03 <kirakosyanmane03@gmail.com>
2024-05-21 20:06:54 +0400

    added the front and back

---
+++ my-project/frontend/src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Escort from "./Escort";
+
+export default function Home() {
+  return (
+    <>
+    <Escort/>
+      <h1>Hello</h1>
+    </>
+  );
+}
