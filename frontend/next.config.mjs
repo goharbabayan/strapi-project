f88d749c32d46c94e5b5cfcc7b54d04d85b2fcc4
Mane Kirakosyan <kirakosyanmane03@gmail.com>
2024-06-25 19:01:50 +0400

    added image configs in next.config file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: ['localhost'], // Add 'localhost' to the allowed domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'snlxdashboard.giesystems.com', // Replace with your Strapi domain
+        pathname: '/uploads/**', // Adjust if your image paths differ
+      },
+    ],
   },
 };
 
